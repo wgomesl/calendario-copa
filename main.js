67f308cb64788ca68c73b2f37c72f30b38abfaf4
Wesley Gomes <wesley.gomes222@hotmail.com>
2022-11-05 23:34:01 -0300

    :tada: primeiro commit

---
+++ main.js
@@ -0,0 +1,54 @@
+function createGame(player1, hour, player2) {
+  return `
+  <li>
+    <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+    <strong>${hour}</strong>
+    <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
+  </li>
+  `
+}
+
+function createCard(date, day, games) {
+  return `
+   <div class="card">
+      <h2>${date} <span>${day}</span></h2>
+      <ul>
+      ${games}
+      </ul>
+    </div>
+  `
+}
+
+document.querySelector("#app").innerHTML = `
+  <header>
+    <img src="./assets/logo.svg" alt="Logo da NLW">
+  </header>
+  <main id="cards">
+  ${createCard(
+    "24/11",
+    "quinta",
+    createGame("uruguay", "10:00", "southkorea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  )}
+  ${createCard(
+    "28/11",
+    "segunda",
+    createGame("southkorea", "10:00", "ghana") +
+      createGame("brazil", "13:00", "switzerland") +
+      createGame("portugal", "16:00", "uruguay")
+  )}
+  ${createCard(
+    "01/12",
+    "quinta",
+    createGame("croatia", "12:00", "belgium") +
+      createGame("japan", "16:00", "spain")
+  )}
+  ${createCard(
+    "02/12",
+    "sexta",
+    createGame("southkorea", "12:00", "portugal") +
+      createGame("brazil", "16:00", "cameroon")
+  )}
+  </main>
+`
